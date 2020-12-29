4712b53d9a92b64ab84dc17fc22063fce05ecd6b
Pranjal Chaplot <pranjalchaplot@gmail.com>
2020-12-30 00:24:15 +0530

    galat commit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,14 +1,30 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 
 export const AddTransaction = () => {
     
     const [Text, setText] = useState('');
     const [Amount, setAmount] = useState(0);
+    const {addTransaction} = useContext(GlobalContext)
+    const onSubmit = e => {
+      e.preventDefault();
+
+      const newTransaction = {
+        id: Math.floor(Math.random()*100000000),
+        Text,
+        Amount
+      }
+
+      addTransaction(newTransaction);
+    }
+
+
+
     return (
         <div>
           <h3>Add new transaction</h3>
-          <form >
+          <form onSubmit={onSubmit}>
             <div className="form-control">
                 <label htmlFor="text">Text</label>
                 <input type="text" value={Text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
@@ -18,9 +34,9 @@ export const AddTransaction = () => {
                 >Amount <br />
                 (negative - expense, positive - income)</label
                 >
-                <input type="number" value={Amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+                <input type="number" value={Amount} onChange={(e) => setAmount(parseInt(e.target.value))} placeholder="Enter amount..." />
             </div>
-            <button className="btn">Add transaction</button>
+            <button className="btn" onClick={addTransaction}>Add transaction</button>
           </form>
         </div>
     )
